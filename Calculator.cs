4fb93a83f8307d199393600ddde215a39e0e1b42
lethihuebh01350 <lehue2055555@gmail.com>
2025-09-25 22:20:18 +0700

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingBasic
+{
+    public class Calculator
+    {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+        public double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
